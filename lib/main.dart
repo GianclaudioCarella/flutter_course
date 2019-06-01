6b7dcab9b599931ce198db15928d320756b6e48e
Gianclaudio Moura Carella <gianclaudiomouracarella@Gianclaudios-MacBook-Air.local>
2019-06-01 01:48:40 -0300

    passing data to stateful widgets

--- lib/main.dart
+++ lib/main.dart
@@ -10,10 +10,10 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-          appBar: AppBar(
-            title: Text('EasyList'),
-          ),
-          body: ProductManager(),
+        appBar: AppBar(
+          title: Text('EasyList'),
+        ),
+        body: ProductManager('Food Tester'),
       ),
     );
   }
