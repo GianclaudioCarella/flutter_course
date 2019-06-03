ad8a2acbcd5167c45357bee18eb958c7f6259894
Gianclaudio <gcarella@pqs.com.ar>
2019-06-03 16:19:52 -0300

    debugging flutter apps

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,16 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+//import 'package:flutter/rendering.dart';
 
 import './product_manager.dart';
 
 void main() => runApp(MyApp());
+
+// to debug
+// void main() {
+//   debugPaintSizeEnabled = true;
+//   runApp(MyApp());  
+// } 
 
 class MyApp extends StatelessWidget {
   @override
