394d74f4316d64b8704772065fd358f73df21f5a
Gianclaudio Moura Carella <gianclaudiomouracarella@Gianclaudios-MacBook-Air.local>
2019-06-24 20:03:17 -0300

    Diving Deeper into Widgets - part 1

--- lib/main.dart
+++ lib/main.dart
@@ -42,9 +42,12 @@ class _MyAppState extends State<MyApp> {
   Widget build(BuildContext context) {
     return MaterialApp(
       theme: ThemeData(
-          brightness: Brightness.dark,
-          primarySwatch: Colors.deepOrange,
-          accentColor: Colors.deepPurple),
+        brightness: Brightness.dark,
+        primarySwatch: Colors.deepOrange,
+        accentColor: Colors.deepPurple,
+        //fontFamily: 'Oswald'
+      ),
+
       //home: AuthPage(),
       routes: {
         '/': (BuildContext context) => AuthPage(),
@@ -60,8 +63,11 @@ class _MyAppState extends State<MyApp> {
         if (pathElements[1] == 'product') {
           final int index = int.parse(pathElements[2]);
           return MaterialPageRoute<bool>(
-            builder: (context) => ProductPage(
-                _products[index]['title'], _products[index]['image']),
+            builder: (BuildContext context) => ProductPage(
+                _products[index]['title'],
+                _products[index]['image'],
+                _products[index]['price'],
+                _products[index]['description']),
           );
         }
         return null;
--- lib/pages/products_admin.dart
+++ lib/pages/products_admin.dart
@@ -22,6 +22,7 @@ class ProductsAdminPage extends StatelessWidget {
                 title: Text('Choose'),
               ),
               ListTile(
+                leading: Icon(Icons.home),
                 title: Text('All Products'),
                 onTap: () {
                   Navigator.pushReplacementNamed(context, '/products');
